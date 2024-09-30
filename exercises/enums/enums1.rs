2c4f55cfdc8281b3e90dc8d3aff055b565715fcf
root <root@carolus-rex>
2024-09-30 14:56:04 +0800

    update

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -7,9 +7,14 @@
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit,
+    Echo,
+    Move,
+    ChangeColor,
 }
 
 fn main() {
+
     println!("{:?}", Message::Quit);
     println!("{:?}", Message::Echo);
     println!("{:?}", Message::Move);
